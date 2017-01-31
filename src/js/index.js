1ffb60cf18121dc80115e1a639ad731733c64bc6
chanchi- <chanchi0707@gmail.com>
2017-02-01 03:48:31 +0900

    TweenMaxの導入

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
+import { TweenMax, TweenLite, ScrollToPlugin, CSSPlugin } from 'gsap'
 import $ from 'jquery';
 import ScrollMagic from 'scrollmagic';
+import setTween from './components/animation.js' //ルート./は絶対いるよ！
 
 console.log('start'); //スタート
 
-let controller = new ScrollMagic.Controller(); 
+let controller = new ScrollMagic.Controller();
+
+let tween = TweenMax.fromTo("#rect", 1, {opacity: 0}, {opacity: 1}); //ターゲット、秒数?
 
 let scene = new ScrollMagic.Scene({triggerElement: "#target", duration: 2000, triggerHook: 'onLeave' })
 	.setPin("#target")
+	.setTween(tween)
 	.addTo(controller);
