6dea9b8b6dbf1bfcc0dbb665b506d67fbf3d123d
chanchi- <chanchi0707@gmail.com>
2017-02-01 02:38:16 +0900

    scrollmagicの導入

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,6 @@ console.log('start'); //スタート
 
 let controller = new ScrollMagic.Controller(); 
 
-let scene = new ScrollMagic.Scene({triggerElement: "#target"})
+let scene = new ScrollMagic.Scene({triggerElement: "#target", duration: 2000, triggerHook: 'onLeave' })
 	.setPin("#target")
 	.addTo(controller);
