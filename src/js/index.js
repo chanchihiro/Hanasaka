f2cf9a61bacfce4feae9a83be481f13f920c66ef
chanchi- <chanchi0707@gmail.com>
2017-02-05 16:00:53 +0900

    debug用のindicatorを導入

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import { TweenMax, TweenLite, ScrollToPlugin, CSSPlugin } from 'gsap'
 import $ from 'jquery';
 import ScrollMagic from 'scrollmagic';
 import setTween from './components/animation.js' //ルート./は絶対いるよ！
+import Indicator from './components/indicator.js'
 
 console.log('start'); //スタート
 
@@ -10,28 +11,40 @@ let txts = [];
 txts = document.getElementsByClassName('txt');
 console.log(txts);
 let hanasaka = document.getElementById('hanasaka');
+console.log(hanasaka);
 
-let txtshow = TweenMax.staggerTo(txts, 100, {opacity:1, transform:'matrix()'}, 20); //対象,かかる時間,
+let txtshow = TweenMax.staggerTo(txts, 100, {opacity:1, transform:'matrix(1,0,0,1,0,0)'}, 20); //対象,かかる時間,
 let txtmove = TweenMax.staggerTo(hanasaka, 30, {right: "-200%"}, 100);
 
-let scene = new ScrollMagic.Scene({
-	triggerElement: '#story', 
-	duration: 30000, 
+// let scene = new ScrollMagic.Scene({
+// 	triggerElement: '#story', 
+// 	duration: 30000, 
+// 	triggerHook: 'onLeave'
+// 	})
+// 	.setTween(txtshow)
+// 	.setPin('#story')
+// 	.addTo(controller);
+
+let scene2 = new ScrollMagic.Scene({
+	triggerElement: '#story',
+	duration: 30000,
 	triggerHook: 'onLeave'
 	})
-	.setTween(txtshow)
+	.setTween(txtmove)
 	.setPin('#story')
-	.addTo(controller);
+	.addTo(controller)
+	.addIndicators({name: 'first'});
 
-$.each(txts, function(i, text) {
-	new ScrollMagic.Scene({
-		triggerElement: text,
-		duration: $(text).width() * 2,
-		offset: $(text).width() * (-2)
-	})
-	.setTween(TweenMax.fromTo( text, 1, 
-		{css: {x: $(text).width()}},
-		{css: {x: 0}}
-	))
-	.addTo(controller);
-});
+// $.each(txts, function(i, text) {
+// 	new ScrollMagic.Scene({
+// 		triggerElement: text,
+// 		duration: $(text).width() * 2,
+// 		offset: $(text).width() * (-2)
+// 	})
+// 	.setTween(TweenMax.fromTo( text, 1, 
+// 		{css: {x: $(text).width()}},
+// 		{css: {x: 0}}
+// 	))
+// 	.setPin('#story')
+// 	.addTo(controller);
+// });
